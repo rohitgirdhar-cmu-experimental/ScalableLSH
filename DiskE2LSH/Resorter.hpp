d19ba4ff042e54faeba45d35245c674cdd2b72a8
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2015-02-08 17:21:32 +0000

    batch wise resorting

--- DiskE2LSH/Resorter.hpp
+++ DiskE2LSH/Resorter.hpp
@@ -4,6 +4,9 @@
 #include "storage/DiskVector.hpp"
 #include <Eigen/Dense>
 #include <algorithm>
+#include <cmath>
+
+#define MAX_RESORT_BATCH_SIZE 10000 // resort these many at a time
 
 class Resorter {
 public:
@@ -19,24 +22,39 @@ public:
       const DiskVector<vector<float>>& feats,
       const vector<float>& qfeat,
       vector<pair<float, int>>& res) {
-    Eigen::MatrixXf qfeat_mat = Eigen::VectorXf::Map(&qfeat[0], qfeat.size());
-    vector<vector<float>> feats_vec;
-    for (auto match = matches.begin(); 
-        match != matches.end(); match++) {
-      vector<float> temp;
-      feats.Get(*match, temp);
-      feats_vec.push_back(temp);
-    }
-    Eigen::MatrixXf feats_mat(matches.size(), qfeat.size());
-    for (int i = 0; i < feats_vec.size(); i++) {
-      feats_mat.row(i) = Eigen::VectorXf::Map(&feats_vec[i][0], feats_vec[i].size());
-    }
-    Eigen::MatrixXf cos_scores = qfeat_mat.transpose() * feats_mat.transpose();
-
     res.clear();
-    int i = 0;
-    for (auto match = matches.begin(); match != matches.end(); match++, i++) {
-      res.push_back(make_pair(cos_scores(i), *match));
+    Eigen::MatrixXf qfeat_mat = Eigen::VectorXf::Map(&qfeat[0], qfeat.size());
+    
+    // Batch process the scoring
+    int nMatches = matches.size();
+    int nBatches = ceil(nMatches * 1.0f / MAX_RESORT_BATCH_SIZE);
+    auto match = matches.begin();
+    vector<vector<float>> feats_vec;
+    vector<pair<float, int>> res_batch;
+    
+    for (int batch = 0; batch < nBatches; batch++) {
+      feats_vec.clear();
+      res_batch.clear();
+      int batchSize = 0;
+      for (; match != matches.end() && batchSize < MAX_RESORT_BATCH_SIZE;
+          match++, batchSize++) {
+        // TODO: Avoid this, use pre-alloc of memory
+        vector<float> temp;
+        feats.Get(*match, temp);
+        feats_vec.push_back(temp);
+        // for output
+        res_batch.push_back(make_pair(0.0f, *match));
+      }
+      Eigen::MatrixXf feats_mat(matches.size(), qfeat.size());
+      for (int i = 0; i < feats_vec.size(); i++) {
+        feats_mat.row(i) = Eigen::VectorXf::Map(&feats_vec[i][0], feats_vec[i].size());
+      }
+      Eigen::MatrixXf cos_scores = qfeat_mat.transpose() * feats_mat.transpose();
+
+      for (int i = 0; i < res_batch.size(); i++) {
+        res_batch[i].first = cos_scores(i);
+      }
+      res.insert(res.end(), res_batch.begin(), res_batch.end());
     }
     sort(res.begin(), res.end());
     reverse(res.begin(), res.end());
