ce2b6da86ea46b6098dccfe5140fbe611e0fd702
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2015-03-10 02:14:25 +0000

    run scripts

--- DiskE2LSH/run_memexgpu_search.sh
+++ DiskE2LSH/run_memexgpu_search.sh
@@ -1,9 +1,11 @@
 CODE_PATH=/home/rgirdhar/data/Work/Code/0002_Retrieval/ScalableLSH/DiskE2LSH
 $CODE_PATH/main.bin \
-    -d /home/rgirdhar/data/Work/Datasets/processed/0001_PALn1KDistractor/features/CNN_pool5_uni_normed_LMDB \
-    -n /home/rgirdhar/data/Work/Datasets/processed/0001_PALn1KDistractor/ImgsList.txt \
-    -c /home/rgirdhar/data/Work/Datasets/processed/0001_PALn1KDistractor/selsearch_boxes/counts.txt \
-    -l /home/rgirdhar/data/Work/Datasets/processed/0001_PALn1KDistractor/search_500_225bit.index \
-    -o /home/rgirdhar/data/Work/Datasets/processed/0001_PALn1KDistractor/matches/ \
-    -m /home/rgirdhar/data/Work/Datasets/processed/0001_PALn1KDistractor/split/TestList.txt
-#    -q /home/rgirdhar/data/Work/Datasets/processed/0001_PALn1KDistractor/features/CNN_pool5_copy \
+    -k 500 \
+    -d /home/rgirdhar/data/Work/Datasets/processed/0004_PALn1KHayesDistractor/features/CNN_pool5_uni_normed_LMDB \
+    -n /home/rgirdhar/data/Work/Datasets/processed/0004_PALn1KHayesDistractor/ImgsList.txt \
+    -c /home/rgirdhar/data/Work/Datasets/processed/0004_PALn1KHayesDistractor/selsearch_boxes/counts.txt \
+    -l /home/rgirdhar/data/Work/Datasets/processed/0004_PALn1KHayesDistractor/search_models/search_250bit.index \
+    -o /home/rgirdhar/data/Work/Datasets/processed/0004_PALn1KHayesDistractor/matches/ \
+    -m /home/rgirdhar/data/Work/Datasets/processed/0004_PALn1KHayesDistractor/split/TrainList.txt
+#-o /home/rgirdhar/data/Work/Datasets/processed/0001_PALn1KDistractor/matches_query/ \
+#-m /home/rgirdhar/data/Work/Datasets/processed/0001_PALn1KDistractor/split/QueryList.txt
